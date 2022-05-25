307674ce1b091370f54a668796df1f0812cfb416
Romain Guilmont <romain.guilmont@besedo.com>
2022-05-25 08:16:43 +0200

    change the way metrics are parsed ( + add tests ), more readable logging and modernize the supported panels

--- main.py
+++ main.py
@@ -1,24 +1,27 @@
+import json
 import logging
 import os
+from functools import reduce
 from typing import Dict
-logging.basicConfig(format='%(asctime)s - %(levelname)s : %(message)s', level=logging.INFO)
-logging.getLogger().setLevel(logging.INFO)
-import json
+
+import coloredlogs
 import requests
+
 import input_validator
-from pygments import highlight
-from pygments.formatters import HtmlFormatter
-from pygments_promql import PromQLLexer
+import prometheus
 
 ALERTS = []
 SUPPORTED_PANELS = ['graph', 'grafana-worldmap-panel', 'grafana-piechart-panel', 'singlestat', 'dashlist',
-                        'alertlist', 'text', 'heatmap', 'bargauge', 'table', 'gauge', 'stat', 'row']
+                        'alertlist', 'text', 'heatmap', 'bargauge', 'table', 'gauge', 'stat', 'row', 'timeseries', 'logs']
+
+logger = logging.getLogger(__name__)
+coloredlogs.install(level='INFO')
 
 # read variable either from environment or, if not found, from input.
 def _read_variable(name: str) -> str:
     if name in os.environ:
         val = os.environ[name]
-        print("Took from environment {}".format(val))
+        logger.info("Took from environment {}".format(val))
         return val
     return input("Enter your {}:".format(name))
 
@@ -33,7 +36,7 @@ def _init_parameters(dashboard, fid):
         dashboard['dashboard']['uid'] = None
         dashboard['dashboard']['refresh'] = "30s"
     except KeyError as e:
-        logging.error(
+        logger.error(
             'At `{}` dashboard, error occurred while setting dashboard parameters'.format(dashboard['dashboard'],
                                                                                           ['title']))
 
@@ -65,7 +68,7 @@ def _create_uploaded_folder(logzio_url: str, logz_api_headers: Dict[str, str]):
     for folder in resp.json():
         if folder['title'] == new_title:
             folder_id = folder['id']
-            logging.info('Found existing `Uploaded by script` folder with id: {}'.format(str(folder_id)))
+            logger.info('Found existing `Uploaded by script` folder with id: {}'.format(str(folder_id)))
     if not folder_id:
         folder_data = {
             "uid": None,
@@ -73,7 +76,7 @@ def _create_uploaded_folder(logzio_url: str, logz_api_headers: Dict[str, str]):
         }
         new_folder = requests.post(url=folder_url, json=folder_data, params={}, headers=logz_api_headers).json()
         folder_id = new_folder['id']
-        logging.info('New folder created with id: {}'.format(str(folder_id)))
+        logger.info('New folder created with id: {}'.format(str(folder_id)))
 
     return folder_id
 
@@ -84,7 +87,7 @@ def _get_panel_types(panels, panel_types):
         try:
             panel_types.append(panel['type'])
         except Exception as e:
-            logging.error(e)
+            logger.error(e)
 
 
 # check for unsupported panel types
@@ -130,81 +133,19 @@ def _clear_notifications(dashboard):
             pass
 
 
-# Recursive function to generate query with the `p8s_logzio_name` label
-def _generate_query(query_string, env_filter_string):
-    if '{' not in query_string:
-        return query_string
-    idx = query_string.index('{') + 1
-    return query_string[:idx] + env_filter_string + _generate_query(query_string[idx:], env_filter_string)
-
-
-def _find_grouping(query_string):
-    grouping_indices = []
-    grouping_statements = ['by(', 'on(', ',', 'group_right(', 'group_left(']
-    for statement in grouping_statements:
-        try:
-            indices = [i for i in range(len(query_string)) if query_string.startswith(statement, i)]
-            for idx in indices:
-                grouping_indices.append(idx + len(statement))
-        except ValueError:
-            pass
-    return grouping_indices
-
-
-def _generate_query_without_filtering(query_string, metric_name, env_filter_string):
-    if metric_name == query_string:
-        return query_string + '{' + env_filter_string.replace(',', '') + '}'
-    if metric_name not in query_string:
-        return query_string
-    idx = query_string.index(metric_name) + len(metric_name)
-    if query_string[idx] != ':':
-        return query_string[:idx] + '{' + env_filter_string.replace(',', '') + '}' + _generate_query_without_filtering(
-            query_string[idx:], metric_name, env_filter_string)
-    else:
-        return query_string[:idx] + _generate_query_without_filtering(query_string[idx:], metric_name,
-                                                                      env_filter_string)
-
-
-def _find_metrics_names(expr):
-    names = []
-    grouping = []
-    splited_query = highlight(expr, PromQLLexer(), HtmlFormatter()).split('"nv">')
-    for ex in splited_query:
-        ex = ex.split('<', 1)
-        if ex[0]:
-            names.append(ex[0])
-    names = list(dict.fromkeys(names))
-    grouping_indices = _find_grouping(expr)
-    if grouping_indices:
-        for name in names:
-            indices = [i for i in range(len(expr)) if expr.startswith(name, i)]
-            for idx in indices:
-                for g_idx in grouping_indices:
-                    if idx == g_idx:
-                        grouping.append(name)
-    return list(set(names) - set(grouping))
-
-
 # Adding `p8s_logzio_name` label to all query strings in the dashboard
 def _add_enviroment_label(panel):
     env_filter_string = 'p8s_logzio_name="$p8s_logzio_name",'
     try:
-        q_idx = 0
-        if not panel['type'] == 'row' and not panel['type'] == 'text':
-            for query in panel['targets']:
-                query_string = query['expr'].replace(' ', '')
-                if '{' in query_string:
-                    new_query = _generate_query(query_string, env_filter_string)
-                    panel['targets'][q_idx]['expr'] = new_query
-                    q_idx += 1
-                else:
-                    metric_names = _find_metrics_names(query_string)
-                    for name in metric_names:
-                        query_string = _generate_query_without_filtering(query_string, name, env_filter_string)
-                    panel['targets'][q_idx]['expr'] = query_string
-                    q_idx += 1
+        if panel['type'] not in ['row', 'text']:
+            new_label = prometheus.LabelFiltering('p8s_logzio_name', '$p8s_logzio_name')
+            def transform_query(acc, target):
+                target['expr'] = prometheus.add_filter(target['expr'], new_label)
+                acc.append(target)
+                return acc
+            panel['targets'] = reduce(transform_query, panel['targets'], [])
     except KeyError as e:
-        logging.error('at _add_enviroment_label: {}'.format(e))
+        logger.error('at _add_enviroment_label: {}'.format(e))
 
 
 def _update_query_variables(var_list, datasource_name):
@@ -235,20 +176,20 @@ def _validate_templating(dashboard):
         dashboard['dashboard']['templating']['list'] = var_list
         _update_panels_datesources(dashboard, datasource_name, var_list)
     except KeyError as e:
-        logging.info('At `{}` dashboard - an error has occurred while editing the dashboard: {}'.format(
+        logger.info('At `{}` dashboard - an error has occurred while editing the dashboard: {}'.format(
             dashboard['dashboard']['title'], e))
 
 
 # main script
 def main():
-    print('Configure your environment variables')
-    print('Your grafana host without protocol specification (e.g. localhost:3000). ')
+    logger.info('Configure your environment variables')
+    logger.info('Your grafana host without protocol specification (e.g. localhost:3000). ')
     GRAFANA_HOST = _read_variable('GRAFANA_HOST')
-    print('Your grafana editor/admin API key, find or create one under Configuration -> API keys.')
+    logger.info('Your grafana editor/admin API key, find or create one under Configuration -> API keys.')
     GRAFANA_TOKEN = _read_variable('GRAFANA_TOKEN')
-    print('Your Logz.io account API token, find it under settings -> tools -> manage tokens -> API tokens.')
+    logger.info('Your Logz.io account API token, find it under settings -> tools -> manage tokens -> API tokens.')
     LOGZIO_API_TOKEN = _read_variable('LOGZIO_API_TOKEN')
-    print('Your Logz.io region code. For example if your region is US, then your region code is `us`. You can find your '
+    logger.info('Your Logz.io region code. For example if your region is US, then your region code is `us`. You can find your '
         'region code here: https://docs.logz.io/user-guide/accounts/account-region.html#regions-and-urls for further '
         'information.')
     REGION_CODE = _read_variable('REGION_CODE')
@@ -291,26 +232,27 @@ def main():
     # init list
     dashboards_list = _init_dashboard_list(uids, BASE_URL, REQUEST_HEADERS)
     # create new folder to store the dashboards
-    folder_id = _create_uploaded_folder()
+    folder_id = _create_uploaded_folder(BASE_API_URL, LOGZIO_API_HEADERS)
 
     for dashboard in dashboards_list:
-        logging.info(f"Processing dashboard {dashboard['dashboard']['title']}")
+        logger.info(f"Processing dashboard {dashboard['dashboard']['title']}")
         if "rows" not in dashboard['dashboard'].keys() or dashboard['dashboard']['schemaVersion'] > 14:
             _init_parameters(dashboard, folder_id)
             _validate_templating(dashboard)
             _inspect_panels_types(dashboard)
             _clear_notifications(dashboard)
             try:
+                
                 upload_response = requests.post(url=UPLOAD_DASHBOARD_URL, data=json.dumps(dashboard), params={},
                                                 headers=LOGZIO_API_HEADERS)
             except Exception as e:
-                logging.error("At `{}` dashboard - upload error : {}".format(dashboard['dashboard']['title'], e))
+                logger.error("At `{}` dashboard - upload error : {}".format(dashboard['dashboard']['title'], e))
             if upload_response.ok:
-                logging.info("`{}` dashboard uploaded successfully, schema version: {}, status code: {}".format(
+                logger.info("`{}` dashboard uploaded successfully, schema version: {}, status code: {}".format(
                     dashboard['dashboard']['title'], dashboard['dashboard']['schemaVersion'],
                     upload_response.status_code))
             else:
-                logging.error(
+                logger.error(
                     'At `{}` dashboard - upload error: {} - schema version: {}'.format(dashboard['dashboard']['title'],
                                                                                        upload_response.text,
                                                                                        dashboard['dashboard'][
@@ -321,7 +263,7 @@ def main():
                 'version ️, current version: {}'.format(dashboard['dashboard']['title'], dashboard[
                     'dashboard']['schemaVersion']))
     for alert in sorted(ALERTS):
-        logging.warning(alert)
+        logger.warning(alert)
 
 
 if __name__ == "__main__":
